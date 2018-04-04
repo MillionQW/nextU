301c23c327f48e13980c98adb1820872cf0dd586
MillionQW <516155382qw@gmail.com>
2018-04-04 09:31:23 +0800

    做了点直播间设置

--- src/components/LiveInfo.vue
+++ src/components/LiveInfo.vue
@@ -2,9 +2,14 @@
 <section>
     <div class="container">
         <h3><icon name="camera-retro" class="camera"></icon>我要直播</h3>
-        
+        <ul class="info-form">
+            <li>房间ID：<span class="liveid">72992</span><button class="btn-enterroom">进入直播间</button></li>
+            <li>房间标题：<span class="roomname">72992</span><button class="btn-modify" @click="modify('roomname')">修改</button></li>
+            <li>房间描述：<textarea name="" id="" cols="70" rows="5"></textarea><button class="btn-modify">修改</button></li>
+            <li>直播类别：<span class="domain">考试</span><button class="btn-modify">修改</button></li>    
+        </ul>
     </div>
-    <input-dialog class="dialog" setkey="类别" value="考试"></input-dialog>
+    <input-dialog ref="dialog" class="dialog" :setkey="setkey" :value="value"></input-dialog>
 </section>
 
 </template>
@@ -15,8 +20,24 @@ import Icon from 'vue-awesome/components/Icon'
 // import InputDialog from './dialog/InputDialog'
 import InputDialog from './dialog/InputDialog'
 export default {
+    data() {
+        return {
+            setkey: '',
+            value: ''
+        }
+    },
     components: {
         Icon, InputDialog
+    },
+    methods:　{
+        modify(type, value) {
+            
+            if (type === 'roomname') {
+                this.setkey = '房间标题';
+                this.value = $('.roomname').html();
+            }
+            this.$refs.dialog.openDialog();
+        }
     }
 }
 </script>
@@ -47,8 +68,32 @@ h3 {
         height: 20px;
     }
 }
-.dialog {
-    position: absolute;
+.info-form {
+    padding: 20px;
+    color: #656766;
+    li {
+        margin-bottom: 40px;
+        span {
+            color: #9a9a9a;
+        }
+        button {
+            margin-left: 20px;
+            font-size: 12px;
+            background: #fff;
+        }
+        .btn-enterroom {
+            padding: 1px 7px;
+            color: #5cc591;
+            border: 1px solid #5cc591;
+            border-radius: 10px;
+        }
+        .btn-modify {
+            padding: 1px 7px;
+            color: #bbb9b9;
+            border: 1px solid #bbb9b9;
+            border-radius: 10px;
+        }
+    }
 }
 
 </style>
--- src/components/dialog/InputDialog.vue
+++ src/components/dialog/InputDialog.vue
@@ -1,30 +1,81 @@
 <template>
-    <div class="dialog-container">
-        <p>修改{{this.setkey}}为：</p>
-        <input type="text" :value="this.value">
-        <div class="dialog-footer">
-            <button> 确定</button>
-            <button>取消</button>
+    <div class="dialog-container" v-show="show">
+        <div class="mask"></div>
+        <div class="dialog-wrapper">
+            <p class="tips">修改{{this.setkey}}为：</p>
+            <input type="text" :value="this.value">
+            <div class="dialog-footer">
+                <button class="submit"> 确定</button>
+                <button class="cancel" @click="closeDialog">取消</button>
+            </div>
         </div>
     </div>
 </template>
 <script>
 export default {
-    props: ['setkey', 'value']
+    data() {
+        return {
+            show: false
+        }
+    },
+    props: ['setkey', 'value'],
+    methods: {
+        openDialog() {
+            this.show = true;
+        },
+        closeDialog() {
+            this.show = false;
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
-.dialog-container {
+.mask {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: #dddee1;
+    opacity: .8;
+    z-index: 1;
+}
+.dialog-wrapper {
+    position: absolute;
     width: 350px;
     height: 100px;
     padding: 20px;
-    background: #854525;
+    background: #fff;
+    border-radius: 5px;
+    z-index: 100;
+    .tips {
+        margin-left: 14px;
+        margin-bottom: 10px;
+    }
+    input {
+        width: 95%;
+        margin-left: 14px;
+        border: 1px solid #dddee1;
+        border-radius: 2px;
+    }
 }
 .dialog-footer {
+    margin-top: 20px;
+    text-align: right;
     button {
         width: 80px;
         height: 40px;
+        color: #fff;
         border-radius: 5px;
+        &:hover {
+            opacity: .8;
+        }
+    }
+    .submit {
+        background: #339966;
+    }
+    .cancel {
+        background: #d24343;
     }
 }
 </style>
